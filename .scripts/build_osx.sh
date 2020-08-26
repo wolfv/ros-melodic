da236e00836166a47dba8298f3759d5c886df91d
Wolf Vollprecht <w.vollprecht@gmail.com>
2020-08-26 19:48:06 +0200

    add osx pipeline

--- .scripts/build_osx.sh
+++ .scripts/build_osx.sh
@@ -50,15 +50,12 @@ conda config --show-sources
 conda list --show-channel-urls
 
 cd ${FEEDSTOCK_ROOT}
-pip install -e .
-
-cd ${FEEDSTOCK_ROOT}/examples
-vinca
+cp recipes/${CURRENT_BUILD_PKG_NAME}.yaml ./recipe.yaml
 
 boa build .
 
-anaconda -t ${ANACONDA_API_TOKEN} upload ${CONDA_BLD_PATH}/osx-64/*.tar.bz2 --force
-quetz-client "${QUETZ_URL}" ${CONDA_BLD_PATH} --force
+# anaconda -t ${ANACONDA_API_TOKEN} upload ${CONDA_BLD_PATH}/osx-64/*.tar.bz2 --force
+# quetz-client "${QUETZ_URL}" ${CONDA_BLD_PATH} --force
 
 # conda build ./recipe -m ./.ci_support/${CONFIG}.yaml --clobber-file ./.ci_support/clobber_${CONFIG}.yaml
 
